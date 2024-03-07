e60c01d78bff8d74bcfb1a340d3af07e4f02c170
aks264 <aks264@cornell.edu>
2024-03-07 16:58:28 -0500

    Help    me

--- data/wrangler.py
+++ data/wrangler.py
@@ -1,4 +1,5 @@
 import csv
+import json
 import re
 
 '''
@@ -20,10 +21,16 @@ import re
 tag_ctr = {}
 nbhd_ctr = {}
 
+restaurants = []
+
 with open("yelp_boston.csv", mode="r") as f:
     csvfile = csv.reader(f)
     
     for line in csvfile:
+
+        # name, categories, neighborhood, location_json, review_structure (rating, count, snippet), url
+
+        categories = []
         
         formatted = re.sub(r"[\[\],.;@#?'!]+", '', line[3])
         formatted_tags = formatted.split('" ')[1::2]
@@ -35,24 +42,41 @@ with open("yelp_boston.csv", mode="r") as f:
                 tag_ctr[tag] += 1
             else:
                 tag_ctr[tag] = 1
+            categories.append(tag)
             
         nbhd = line[7]
         if line[7] in nbhd_ctr.keys():
             nbhd_ctr[line[7]] += 1
         else:
             nbhd_ctr[line[7]] = 1
-            
+
+        rest_obj = {
+            "name" : line[0],
+            "categories" : categories,
+            "neighborhood" : line[7],
+            "loc_json" : line[6],
+            "review_json" : {
+                "rating" : line[4],
+                "count" : line[2],
+                "snippet" : line[5]
+            },
+            "url": line[1]
+        }
+
+        restaurants.append(rest_obj)            
 
 
 tag_ctr_sorted = dict(sorted(tag_ctr.items(), key=lambda item : item[1], reverse=True))
 nbhd_ctr_sorted = dict(sorted(nbhd_ctr.items(), key=lambda item : item[1], reverse=True))
 
 
-print(len(tag_ctr_sorted.keys()))
+# print(len(tag_ctr_sorted.keys()))
 
-print(tag_ctr_sorted)
+# print(tag_ctr_sorted)
 
-print(len(nbhd_ctr_sorted.keys()))
+# print(len(nbhd_ctr_sorted.keys()))
 
-print(nbhd_ctr_sorted)
+# print(nbhd_ctr_sorted)
 
+with open("bos_rest_formatted.json", "w") as f:
+    json.dump(restaurants[1:], f) 
